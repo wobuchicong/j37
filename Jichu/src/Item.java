90b864c01805d64c6bbf64351f39e2dfc7f660aa
qiubei <958109549@qq.com>
2019-08-09 09:59:59 +0800

    第一次上传到git

---
+++ Jichu/src/Item.java
@@ -0,0 +1,17 @@
+
+public class Item {
+	public String name;
+	public int price;
+	public static void main(String[] args) {
+		Item hp=new Item();
+		hp.name="ÑªÆ¿";
+		hp.price=50;
+		Item shoes=new Item();
+		shoes.name="²ÝÐ¬";
+		shoes.price=300;
+		Item Longsword=new Item();
+		Longsword.name="³¤½£";
+		Longsword.price=350;
+	}
+
+}
